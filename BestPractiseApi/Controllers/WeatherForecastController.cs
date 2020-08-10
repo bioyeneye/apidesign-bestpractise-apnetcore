14eee75831ddf536ed30cfaabd13c7b7b06e404d
Bolaji Oyeneye <bioyeneye@gmail.com>
2020-08-10 06:10:22 +0100

    cleanup

--- BestPractiseApi/Controllers/WeatherForecastController.cs
+++ BestPractiseApi/Controllers/WeatherForecastController.cs
@@ -24,16 +24,25 @@ namespace BestPractiseApi.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public ActionResult Get(int limit=10, int offset=0)
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var data = Enumerable.Range(1, 100).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .Skip(offset)
+                .Take(limit)
+                .ToArray();
+
+            return Ok();
         }
+    }
+
+    public class PaginatedResult
+    {
+        
     }
 }
